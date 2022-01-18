4458dbed002dae50df04a561005bcca78ca27f8d
Clifford Tse <TseClifford@gmail.com>
2022-01-18 20:44:08 +0000

    added animatedStr function

---
+++ typewriter.js
@@ -0,0 +1,18 @@
+const sentence = "hello there from lighthouse labs";
+
+const animatedStr = (string) => {
+  for (let i = 0; i < string.length; i++) {
+    if (i === string.length - 1) {
+      setTimeout(() => {
+        process.stdout.write(`${string[i]}\n`);
+      }, i * 50)
+    }
+    else {
+      setTimeout(() => {
+        process.stdout.write(`${string[i]}`);
+      }, i * 50)
+    }
+  }
+}
+
+animatedStr(sentence)
